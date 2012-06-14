721591bb16ce89b8b106988fa42711f2bce194d1
ben-oxley <ben.oxley@gmail.com>
2012-06-14 12:51:59 +0100

    Started implementation of TinyGPS library

--- altisphere4_0.ino
+++ altisphere4_0.ino
@@ -34,101 +34,114 @@
  
  Ideal analog divider is 10K and 27K   730, 378, 3
  */
- 
+
 /*
   GPS Level Convertor Board Test Script
-  03/05/2012 2E0UPU
+ 03/05/2012 2E0UPU
  
-  Initialise the GPS Module in Flight Mode and then echo's out the NMEA Data to the Software Serial.
+ Initialise the GPS Module in Flight Mode and then echo's out the NMEA Data to the Software Serial.
  
-  This example code is in the public domain.
-  Additional Code by J Coxon (http://ukhas.org.uk/guides:falcom_fsa03)
+ This example code is in the public domain.
+ Additional Code by J Coxon (http://ukhas.org.uk/guides:falcom_fsa03)
  */
- 
+
+#include "TinyGPS.h"
+TinyGPS gps;
 #include <SoftwareSerial.h>
- 
+
 SoftwareSerial mySerial(4, 5);
- 
+
 byte gps_set_sucess = 0 ;
- 
+
 void setup()
 {
   mySerial.begin(9600);
   Serial.begin(9600);
   mySerial.println("Initialising....");
- 
- // THIS COMMAND SETS FLIGHT MODE AND CONFIRMS IT 
+
+  // THIS COMMAND SETS FLIGHT MODE AND CONFIRMS IT 
   mySerial.println("Setting uBlox nav mode: ");
   uint8_t setNav[] = {
-    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC                      };
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC                        };
   while(!gps_set_sucess)
   {
     sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
     gps_set_sucess=getUBX_ACK(setNav);
   }
   gps_set_sucess=0;
- 
+
   // THE FOLLOWING COMMANDS DO WHAT THE $PUBX ONES DO BUT WITH CONFIRMATION
-   debug.println("Switching off NMEA GLL: ");
-   uint8_t setGLL[] = { 
-   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B                   };
-   while(!gps_set_sucess)
-   {		
-   sendUBX(setGLL, sizeof(setGLL)/sizeof(uint8_t));
-   gps_set_sucess=getUBX_ACK(setGLL);
-   }
-   gps_set_sucess=0;
- 
-   debug.println("Switching off NMEA GSA: ");
-   uint8_t setGSA[] = { 
-   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32                   };
-   while(!gps_set_sucess)
-   {	
-   sendUBX(setGSA, sizeof(setGSA)/sizeof(uint8_t));
-   gps_set_sucess=getUBX_ACK(setGSA);
-   }
-   gps_set_sucess=0;
-   debug.println("Switching off NMEA GSV: ");
-   uint8_t setGSV[] = { 
-   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39                   };
-   while(!gps_set_sucess)
-   {
-   sendUBX(setGSV, sizeof(setGSV)/sizeof(uint8_t));
-   gps_set_sucess=getUBX_ACK(setGSV);
-   }
-   gps_set_sucess=0;
-   debug.print("Switching off NMEA RMC: ");
-   uint8_t setRMC[] = { 
-   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x04, 0x40                   };
-   while(!gps_set_sucess)
-   {
-   sendUBX(setRMC, sizeof(setRMC)/sizeof(uint8_t));
-   gps_set_sucess=getUBX_ACK(setRMC);
-   }
-   gps_set_sucess=0;
-   debug.print("Switching off NMEA VTG: ");
-   uint8_t setVTG[] = { 
-   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x46                   };
-   while(!gps_set_sucess)
-   {
-   sendUBX(setVTG, sizeof(setRMC)/sizeof(uint8_t));
-   gps_set_sucess=getUBX_ACK(setVTG);
- 
-   }
-   
- 
+  debug.println("Switching off NMEA GLL: ");
+  uint8_t setGLL[] = { 
+    0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B                     };
+  while(!gps_set_sucess)
+  {		
+    sendUBX(setGLL, sizeof(setGLL)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setGLL);
+  }
+  gps_set_sucess=0;
+
+  debug.println("Switching off NMEA GSA: ");
+  uint8_t setGSA[] = { 
+    0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32                     };
+  while(!gps_set_sucess)
+  {	
+    sendUBX(setGSA, sizeof(setGSA)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setGSA);
+  }
+  gps_set_sucess=0;
+  debug.println("Switching off NMEA GSV: ");
+  uint8_t setGSV[] = { 
+    0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39                     };
+  while(!gps_set_sucess)
+  {
+    sendUBX(setGSV, sizeof(setGSV)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setGSV);
+  }
+  gps_set_sucess=0;
+  debug.print("Switching off NMEA RMC: ");
+  uint8_t setRMC[] = { 
+    0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x04, 0x40                     };
+  while(!gps_set_sucess)
+  {
+    sendUBX(setRMC, sizeof(setRMC)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setRMC);
+  }
+  gps_set_sucess=0;
+  debug.print("Switching off NMEA VTG: ");
+  uint8_t setVTG[] = { 
+    0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x46                     };
+  while(!gps_set_sucess)
+  {
+    sendUBX(setVTG, sizeof(setRMC)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setVTG);
+
+  }
+
+
 }
- 
 void loop()
 {
-    Serial.println("$PUBX,00*33");
-    if(Serial.available() > 0) 
-    {
-      char inByte = Serial.read();
-      mySerial.write(inByte);
- 
-    }
- 
+  while (nss.available())
+  {
+    int c = nss.read();
+
+  }
+}
+
+void loop()
+{
+  Serial.println("$PUBX,00*33");
+  if(Serial.available() > 0) 
+  {
+    char inByte = Serial.read();
+    mySerial.write(inByte);
+  }
+  if (gps.encode(c))
+  {
+    // process new gps info here
+  }
+
 
 }     
 // Send a byte array of UBX protocol to the GPS
@@ -139,8 +152,8 @@ void sendUBX(uint8_t *MSG, uint8_t len) {
   }
   Serial.println();
 }
- 
- 
+
+
 // Calculate expected UBX ACK packet and parse UBX response from GPS
 boolean getUBX_ACK(uint8_t *MSG) {
   uint8_t b;
@@ -148,7 +161,7 @@ boolean getUBX_ACK(uint8_t *MSG) {
   uint8_t ackPacket[10];
   unsigned long startTime = millis();
   mySerial.print(" * Reading ACK response: ");
- 
+
   // Construct the expected ACK packet    
   ackPacket[0] = 0xB5;	// header
   ackPacket[1] = 0x62;	// header
@@ -160,32 +173,32 @@ boolean getUBX_ACK(uint8_t *MSG) {
   ackPacket[7] = MSG[3];	// ACK id
   ackPacket[8] = 0;		// CK_A
   ackPacket[9] = 0;		// CK_B
- 
+
   // Calculate the checksums
   for (uint8_t i=2; i<8; i++) {
     ackPacket[8] = ackPacket[8] + ackPacket[i];
     ackPacket[9] = ackPacket[9] + ackPacket[8];
   }
- 
+
   while (1) {
- 
+
     // Test for success
     if (ackByteID > 9) {
       // All packets in order!
       mySerial.println(" (SUCCESS!)");
       return true;
     }
- 
+
     // Timeout if no valid response in 3 seconds
     if (millis() - startTime > 3000) { 
       mySerial.println(" (FAILED!)");
       return false;
     }
- 
+
     // Make sure data is available to read
     if (Serial.available()) {
       b = Serial.read();
- 
+
       // Check that bytes arrive in sequence as per expected ACK packet
       if (b == ackPacket[ackByteID]) { 
         ackByteID++;
@@ -194,7 +207,8 @@ boolean getUBX_ACK(uint8_t *MSG) {
       else {
         ackByteID = 0;	// Reset and look again, invalid order
       }
- 
+
     }
   }
 }
+
